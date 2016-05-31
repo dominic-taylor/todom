16b9608628784a898af3236b13372f1a5a5ff36d
dominic-taylor <dataylorx@gmail.com>
2016-05-31 14:43:17 +1200

    add form submit adds to list

--- client/app.js
+++ client/app.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    console.log("DOM fully loaded and parsed");
 
+   var taskList = document.getElementById('list');
+   taskList.addEventListener('click', function (event) {
+
+     if (event.target.tagName === 'LI' && event.target.style.backgroundColor != 'rgba(0, 188, 212, 0.631373)') {
+       event.target.style.backgroundColor = 'rgba(0, 188, 212, 0.63)'
+     }
+     else {
+       event.target.style.backgroundColor = ''
+     }
+   }, false);
+
+   var taskDoneBtn = document.getElementById('done');
+   taskDoneBtn.addEventListener("click", removeTask, false);
+
+   var taskForm = document.getElementById('taskForm')
+   taskForm.addEventListener("submit", function(e){
+     e.preventDefault()
+     addNewTodo()
+   }, false)
+
 function addNewTodo(){
   var newTask = document.querySelector('input[type=text]').value
   var newTodo = document.createElement('li')
@@ -13,28 +33,14 @@ function addNewTodo(){
 function removeTask() {
   var list = document.querySelector('ol')
   var doneItems = document.getElementsByTagName('li')
-  var looplen = doneItems.length;
-  for(var i =0; i<looplen; i++) {
+  var listLength = doneItems.length;
+  var i = 0;
+  while(i<listLength) {
     if (doneItems[i].style.backgroundColor == 'rgba(0, 188, 212, 0.631373)'){
       list.removeChild(doneItems[i]);
     }
+    i++
   }
 }
 
-var listAdder = document.getElementById('add');
-listAdder.addEventListener("click", addNewTodo, false);
-
-var taskList = document.getElementById('list');
-taskList.addEventListener('click', function (event) {
-
-    if (event.target.tagName === 'LI' && event.target.style.backgroundColor != 'rgba(0, 188, 212, 0.631373)') {
-        event.target.style.backgroundColor = 'rgba(0, 188, 212, 0.63)'
-    }
-    else {
-      event.target.style.backgroundColor = ''
-    }
-}, false);
-
-var doneBtn = document.getElementById('done');
-doneBtn.addEventListener("click", removeTask, false);
 }); //DOM ready braces
