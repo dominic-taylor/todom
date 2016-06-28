cdb2f07f7d361de0298020512cdd88e13163aef2
dominic-taylor <dataylorx@gmail.com>
2016-06-28 16:26:34 +1200

    Add check if username exists before signup

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
 var Knex = require('knex')
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
 var flash = require('connect-flash')
 var path = require('path')
 
@@ -29,51 +27,25 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(session({
   secret: 'ilovesecrets', resave: true, saveUninitialized: true}))
-app.use(passport.initialize())
-app.use(passport.session())
 app.use(flash())
 
-// How to login properly? does not seem to hit this at all.
-passport.use('local-login', new LocalStrategy(
-  function(username, password, done) {
-      console.log('login process: ', username);
-      return done('SELECT user_name FROM accounts WHERE user_name=$1 AND user_pass=$2', username, password)
-    .then( function (result){
-      console.log('login sucess ', username);
-      return done(null, result)
-    })
-  .catch( function (err){
-      console.log("/login: " + err);
-      return done(null, false, {message: 'wrong username or password'})
-    })
-}))
-
-passport.serializeUser(function(user, done){
-  done(null, user.user_id)
+app.get('/logout', function (req, res) {
+  console.log('logging out...');
+  req.session.destroy()
+  res.redirect('client/index.html')
 })
 
-passport.deserializeUser(function(id, done) {
-  done("SELECT user_id, user_name FROM accounts user_id=$1", id)
-  .then( function(user){
-    done(null, user)
-  })
-  .catch(function (err){
-    done(new Error('User with the id ${id} does not exist'))
-  })
+app.get('/', function (req, res){
+  res.redirect('client/index.html')
 })
-
-//
-// app.post('/login',
-//   passport.authenticate('local-login', { successRedirect: '/',
-//                                    failureRedirect: '/fail.html',
-//                                    failureFlash: true})
-// )
 
 app.post('/login', function (req, res) {
+
   knex('accounts').where({user_name: req.body.name})
   .then (function (data) {
     console.log('userdata ', data);
     if (req.body.name === '') {
+      console.log('no user name!');
       res.redirect('/')
     }
     else if (req.body.pass == data[0].hash){
@@ -92,36 +64,40 @@ app.post('/login', function (req, res) {
     res.sendStatus(403)
   })
 })
-app.get('logout', function (req, res) {
-  req.session.destroy()
-  res.redirect('/')
-})
-app.get('/', function (req, res){
-  res.redirect('client/index.html')
-})
+
+
 
 
 app.post('/signup', function (req, res) {
-  console.log('user written ', req.body.name, req.body.pass);
-  knex('accounts')
-    .insert({user_name: req.body.name, hash: req.body.pass})
-    .then(function (data) {
-      console.log('signup data ', data);
-      req.session.userId = data[0].id
-      req.session.name = req.body.name
+  var usernameTaken = false;
 
-      console.log('user written ', req.session);
-      res.redirect('/')
-    })
+  knex.select('user_name').from('accounts')
+      .then(function (data){
+        for(var i=0; i<data.length; i++){
+          if (data[i].user_name == req.body.name){
+            console.log('data.username taken ', data[i].user_name);
+            usernameTaken = true;
+          }
+        }
+        if(!usernameTaken){
+          console.log(data.length);
+          req.session.name = req.body.name
+          req.session.userId = data.length + 1
+          console.log('req.ses.uId ', req.session.userId);
+          return knex('accounts').insert({user_name: req.body.name, hash: req.body.pass})
+        }
+      })
+
+ res.redirect('/')
 })
 
 app.get('/api/v1/tasks', function (req, res) {
-  console.log('req.session.user ', req.session.userId);
+  console.log('req.session.userId ', req.session.userId);
   knex('accounts')
-  .join('tasks', 'user_name', '=', 'tasks.user')
-  .select('*').where('userId', req.session.userId)//username:req.user ( ? )
+  .join('tasks', 'id', '=', 'tasks.userId')
+  .select('*').where('userId', req.session.userId) //tasks.userId?
   .then(function(data){
-    console.log('i think it is getting, but no saved data yet');
+    console.log('i think it is getting');
     res.send(data)
   })
   .catch(function (err) {
@@ -132,10 +108,12 @@ app.get('/api/v1/tasks', function (req, res) {
 app.post('/api/v1/save', function (req, res){
     var taskArr = req.body.tasks
     console.log('taskArr ', taskArr);
+    console.log('req.ses ', req.session);
     knex('tasks')
-      .insert({task: taskArr, user: req.session.name, userId: req.session.userId})
+      .update({task: taskArr, user: req.session.name, userId: req.session.userId})
       .then(function (data) {
-      console.log("Tasks saved")
+      console.log("Tasks saved for username ",req.session.name )
+      console.log('and userId ', req.session.userId);
     })
     .catch(function(err){
       console.log(err);
